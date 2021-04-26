5ac9c8daaabbe2a4ba3facc866f7cccc14e6cc31
Lucky Sharma <lucky.sharma@tothenew.com>
2021-04-26 16:39:27 +0530

    Initial commit

---
+++ src/main/java/Ques2.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+
+public class Ques2 {
+    public static void main(String[] args) {
+
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+
+            driver.get("https://www.thehindu.com/");
+            LogEntries entry = driver.manage().logs().get(LogType.BROWSER);
+            List<LogEntry> logs = entry.getAll();
+
+            for (LogEntry e : logs) {
+                System.out.println(e);
+            }
+
+            for (LogEntry e : logs) {
+                System.out.println("Message is: " + e.getMessage());
+                System.out.println("Level is: " + e.getLevel());
+            }
+        }
+    }
